f3f28426aa36194d80ed703516a31f56da951496
Matthias Lindner <matthiaslindner@icloud.com>
2025-08-16 15:36:25 +0200

    auth test

--- app/api/sign-upload/route.ts
+++ app/api/sign-upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { supabaseAdmin, SUPA_BUCKET_ORIG } from "@/lib/supabase/client";
+import { supabaseAdmin, SUPA_BUCKET_ORIG } from "@/lib/supabase/admin";
 
 export async function POST(req: NextRequest) {
   try {
